dd93b4c4437ce6243c2494dff74dae940aef7251
Deyan Hadzhiev <deyan.z.hadzhiev@gmail.com>
2018-03-17 13:41:18 +0200

    Add bash script for setting up SDL on Windows
    The scrpt downloads the official SDL-1.2.15 zip file to the SDK
    directory and extracts it where it will be required by build projects.
    
    The script is fully compatible with git bash on Windows

---
+++ scripts/sdl_win.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ -z $BASH_SOURCE ]; then
+	echo "ERROR \$BASH_SOURCE is required"
+	exit 1
+fi
+
+BASH_DIR=$(dirname $BASH_SOURCE)
+SDK_DIR=$BASH_DIR/../SDK
+
+if [ ! -d $SDK_DIR ]; then
+	mkdir $SDK_DIR
+fi
+
+echo ">> Downloading SDL 1.2 into $SDK_DIR"
+curl https://www.libsdl.org/release/SDL-devel-1.2.15-VC.zip --output $SDK_DIR/sdl.zip
+
+if [ $? -ne 0 ]; then
+	echo ">> ERROR: cURL failed to download SDL sdk"
+else
+
+	echo ">> Extracting SDL sdk in $SDK_DIR/SDL"
+	# unzip doesn't handle relative paths well
+	unzip -u $SDK_DIR/sdl.zip -d $SDK_DIR
+
+	mv $SDK_DIR/SDL-1.2.15 $SDK_DIR/SDL
+
+	rm $SDK_DIR/sdl.zip
+fi
