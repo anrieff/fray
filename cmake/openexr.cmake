1796ea4b7b05b5d477b212427ae9bdff5d1b1608
Deyan Hadzhiev <deyan.z.hadzhiev@gmail.com>
2018-06-02 22:16:24 +0300

    Fix CMake configuration on Linux
    * Change SDL inclusion. Since it is properly found with the find package
      function, only the name of the linked library had to be changed,
      because the SDL library does not need to be 'imported'. Also the
      shared library copying command had to be enabled only for Windows,
      since the SDL shared library is usually installed in one of the
      default load locations for ld.
    * Reimplement importing of the OpenEXR library. There is no single
      standard find package implementation for OpenEXR, so the only option
      avoiding additional installations and repository clonings, is to find
      the library manually. Simple search in the implicitly defined linker
      directories is performed for the main shared library, and then the
      path is reused to define all the required libraries, as dependency of
      the main imported one.
    * Disable Zlib linking and inclusion for Linux/OSX. It is required only
      by the non-shared version of OpenEXR, while the default installations
      on Linux/OSX are shared by default, so they already include zlib.

--- cmake/openexr.cmake
+++ cmake/openexr.cmake
@@ -25,6 +25,42 @@ if (WIN32)
 	# Now add all the libraries created for the OpenEXR dependecy
 	set_target_properties(${OPENEXR_LIB} PROPERTIES INTERFACE_LINK_LIBRARIES "${OPENEXR_LIBS_LIST}")
 else ()
-	# TODO: test
-	find_package(OpenEXR REQUIRED)
+	set_target_properties(${OPENEXR_LIB} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
+		"/usr/include/OpenEXR"
+	)
+
+	# This is totally hacky way of using default installation of OpenEXR, but due to the lack of
+	# default cmake find package implementation, there is not much choice.
+	# Since openexr libraries reside in implicity library locations, used by the compiler, to
+	# obtain the full file name of the exact library, we need to check its existance in all
+	# implicitly defined library directories, until we find one or fail finding any.
+	set(_OpenExrLibPath "unknown")
+	foreach(_implicit_path ${CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES})
+		if (EXISTS "${_implicit_path}/libIlmImf.so")
+			set(_OpenExrLibPath ${_implicit_path})
+			break()
+		endif()
+	endforeach()
+	if (_OpenExrLibPath STREQUAL "unknown")
+		message(FATAL_ERROR "OpenExr library was not found in default library locations")
+	endif()
+
+	set_target_properties(${OPENEXR_LIB} PROPERTIES IMPORTED_LOCATION "${_OpenExrLibPath}/libIlmImf.so")
+
+	set(OPENEXR_DEPENDENCY_LIST
+		"Half"
+		"Iex"
+		"IlmThread"
+	)
+
+	set(OPENEXR_LIBS_LIST "")
+	foreach(exr_target ${OPENEXR_DEPENDENCY_LIST})
+		add_library(${exr_target} STATIC IMPORTED)
+		set_target_properties(${exr_target} PROPERTIES IMPORTED_LOCATION "${_OpenExrLibPath}/lib${exr_target}.so")
+		list(APPEND OPENEXR_LIBS_LIST ${exr_target})
+	endforeach(exr_target)
+
+	# Now add all the libraries created for the OpenEXR dependecy
+	set_target_properties(${OPENEXR_LIB} PROPERTIES INTERFACE_LINK_LIBRARIES "${OPENEXR_LIBS_LIST}")
 endif ()
+
--- cmake/sdl.cmake
+++ cmake/sdl.cmake
@@ -1,8 +1,10 @@
 
-set(SDL_LIB SDL)
-add_library(${SDL_LIB} STATIC IMPORTED)
+set(SDL_LIB SDL_lib)
 
 if (WIN32)
+	# On Windows the SDL library is linked as an imported one.
+	add_library(${SDL_LIB} STATIC IMPORTED)
+
 	set_target_properties(${SDL_LIB} PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
 		"${CMAKE_CURRENT_SOURCE_DIR}/../SDK/SDL/include"
 	)
@@ -10,6 +12,8 @@ if (WIN32)
 	set_target_properties(${SDL_LIB} PROPERTIES IMPORTED_LOCATION "${CMAKE_CURRENT_SOURCE_DIR}/../SDK/SDL/lib/x64/SDL.lib")
 	set_target_properties(${SDL_LIB} PROPERTIES DLL_FILE_LIST "${CMAKE_CURRENT_SOURCE_DIR}/../SDK/SDL/lib/x64/SDL.dll")
 else ()
-	# TODO: test
 	find_package(SDL REQUIRED)
+
+	set(SDL_LIB ${SDL_LIBRARY})
 endif ()
+
